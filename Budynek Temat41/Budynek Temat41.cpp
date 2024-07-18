d063386affa074df94a45ff56540b5057d8d2e74
Vetal82 <havevetal@gmail.com>
2024-07-18 20:54:01 +0200

    Redaction class Main

--- Budynek Temat41/Budynek Temat41.cpp
+++ Budynek Temat41/Budynek Temat41.cpp
@@ -7,14 +7,14 @@ int main() {
     setlocale(LC_ALL, "Ukr");
 
     try {
-        Apartment apartment(101, 100.0, 50.0); // створити об'єкт класу Apartment
-        Office office("ABC Inc.", "IT", 1000.0, &apartment); // створити об'єкт класу Office з посиланням на об'єкт класу Apartment
+        Apartment apartment(101, 100.0, 50.0); 
+        Office office("ABC Inc.", "IT", 1000.0, &apartment); 
 
         std::cout << "Назва фірми-власника: " << office.getCompanyName() << std::endl;
         std::cout << "Вид діяльності: " << office.getActivityType() << std::endl;
         std::cout << "Щомісячна оплата: " << office.getMonthlyPayment() << " грн" << std::endl;
 
-        // доступ до об'єкта класу Apartment через посилання
+        
         std::cout << "Номер квартири: " << office.getApartment()->getNumber() << std::endl;
         std::cout << "Площа: " << office.getApartment()->getArea() << std::endl;
 
